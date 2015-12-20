b174ddeb99ba9bc909d9be77525f0daba1342a0d
jonschlinkert <jon.schlinkert@sellside.com>
2015-12-20 14:05:40 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,59 @@
+/*!
+ * affirmative <https://github.com/jonschlinkert/affirmative>
+ *
+ * Copyright (c) 2015, Jon Schlinkert.
+ * Licensed under the MIT License.
+ */
+
+'use strict';
+
+module.exports = [
+  'absolutely',
+  'affirmative',
+  'all right',
+  'amen',
+  'aye',
+  'beyond a doubt',
+  'by all means',
+  'certainly',
+  'definitely',
+  'even so',
+  'exactly',
+  'fine',
+  'gladly',
+  'good enough',
+  'good',
+  'granted',
+  'i accept',
+  'i concur',
+  'i guess',
+  'if you must',
+  'indubitably',
+  'just so',
+  'most assuredly',
+  'naturally',
+  'of course',
+  'okay',
+  'positively',
+  'precisely',
+  'right on',
+  'righto',
+  'sure thing',
+  'sure',
+  'surely',
+  'true',
+  'undoubtedly',
+  'unquestionably',
+  'very well',
+  'whatever',
+  'willingly',
+  'without fail',
+  'y',
+  'yea',
+  'yeah',
+  'yep',
+  'yes',
+  'yessir'
+];
+
+
